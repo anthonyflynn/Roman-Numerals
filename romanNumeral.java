ca3eeeb9418722795193ab1c2e987e93ca97dcef
Anthony Flynn <anthonymilesflynn@gmail.com>
2016-03-13 06:20:29 +0000

    Added methods: output() and run()

--- romanNumeral.java
+++ romanNumeral.java
@@ -9,19 +9,36 @@ public class romanNumeral {
 	private final Scanner scanner;
 	private final PrintStream out;
 	private int inputValue;
-	//REMEMBER TO CLOSE THE SCANNER
+	private String romanNumeral;
 	
 	/* Constructor to initialise parameter values */
 	public romanNumeral(InputStream in, PrintStream out) {
 		scanner = new Scanner(in);
 		this.out = out;
 		inputValue = Integer.MAX_VALUE; // to determine if valid value entered
+		romanNumeral = "";
 	}
 	
+	/* Method to run the converter */
+	public void run() {		
+		while(true) { // loop while user has more values to convert
+			if(!this.input()) { // false when user types -1 (to exit program)
+				break;
+			}
+			this.romanNumeralEquivalent(this.inputValue);
+			if(!this.output()) { // false when user do not have another value to convert
+				break;
+			}
+		}
+		
+		scanner.close(); // close input stream
+		out.close(); // close output stream
+	}
 	
+	/* Main method */
 	public static void main(String[] args) {
 		romanNumeral example = new romanNumeral(System.in, System.out);
-		example.input();
+		example.run(); // start the romanNumeral converter object
 	}
 	
 	
@@ -36,6 +53,7 @@ public class romanNumeral {
 				int temp = scanner.nextInt();
 				
 				if(temp == -1) { // user wants to exit
+					out.println("Exiting program.");
 					return false;
 				}
 				else if(temp > -1 && temp <= 3999) { // valid input entered
@@ -56,33 +74,58 @@ public class romanNumeral {
 		return true;
 	}
 	
-	/* getter function to return value*/
+	/* Method outputs the roman numeral equivalent of input integer.  If user would like
+	 * to convert another integer, method returns true.  Otherwise, false.*/
+	public boolean output() {
+		out.println("The equivalent value in Roman Numerals is: " + romanNumeral);
+		
+		try {
+			out.println("Would you like to convert another value? (y/n)");
+			String repeat = scanner.next();
+			if(repeat.charAt(0) == 'y' || repeat.charAt(0) == 'Y') {
+				inputValue = Integer.MAX_VALUE;
+				romanNumeral = "";
+				return true;
+			}
+			else if(repeat.charAt(0) == 'n' || repeat.charAt(0) == 'N') {
+				out.println("Exiting program.");
+			}
+			else {
+				out.println("Command not understood, exiting program.");
+			}
+		} catch(Exception e) {
+			out.println("Command not understood, exiting program.");	
+		}	
+		return false;
+	}
+	
+	/* Getter method to return value*/
 	public int getValue() {
 		return inputValue;
 	}
 	
-	/* Stub to be completed later once get functions written */
+	/* Method converts inputValue to its roman numeral equivalent and assigns
+	 * this to the string romanNumeral*/
 	public String romanNumeralEquivalent(int inputValue) {
-		String romanNumeral = "";
 		int unit = 0;
 		
 		unit = inputValue / 1000;
 		inputValue = inputValue % 1000;
-		romanNumeral += getThousands(unit);
+		romanNumeral += getThousands(unit); // adds thousands part
 		
 		unit = inputValue / 100;
 		inputValue = inputValue % 100;
-		romanNumeral += getHundreds(unit);
+		romanNumeral += getHundreds(unit); // adds hundreds part
 		
 		unit = inputValue / 10;
 		inputValue = inputValue % 10;
-		romanNumeral += getTens(unit);
-		romanNumeral += getOnes(inputValue);
+		romanNumeral += getTens(unit); // adds tens part
+		romanNumeral += getOnes(inputValue); // remainder is units part
 		
 		return romanNumeral;
 	}
 	
-	/* Function to return the value in the single units column of input*/
+	/* Method to return the value in the single units column of input*/
 	public String getOnes(int inputValue) {
 		switch(inputValue) {
 		case 0: return "";
@@ -102,7 +145,7 @@ public class romanNumeral {
 		}
 	}
 	
-	/* Function to return the value in the tens units column of input*/
+	/* Method to return the value in the tens units column of input*/
 	public String getTens(int inputValue) {
 		switch(inputValue) {
 		case 0: return "";
@@ -122,7 +165,7 @@ public class romanNumeral {
 		}
 	}
 	
-	/* Function to return the value in the hundred units column of input*/
+	/* Method to return the value in the hundreds units column of input*/
 	public String getHundreds(int inputValue) {
 		switch(inputValue) {
 		case 0: return "";
@@ -142,6 +185,7 @@ public class romanNumeral {
 		}
 	}
 	
+	/* Method to return the value in the thousands units column of input*/
 	public String getThousands(int inputValue) {
 		String thousandsString = "";
 		for(int i = 0; i < inputValue; i++) {
